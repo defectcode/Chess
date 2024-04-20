70ea9621db9ba3c8bc9ebf0239e51ea5ef93b6c8
Constantin <paladuta05@gmail.com>
2024-04-20 12:21:42 +0300

    add element

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,16 +3,20 @@ import { Cell } from "../models/Cell";
 import { FC } from "react";
 
 interface CellProps {
-    cell: Cell
+    cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
         
         <div 
-            className={['cell', cell.color].join(' ')}
+            className={['cell', cell.color, selected ? "selected" : ''].join(' ')}
+            onClick={() => click(cell)}
+            style={{background: cell.available && cell.figure ? 'green' : ''}}
         >
-
+            {cell.available && !cell.figure &&<div className="available"></div>}
             {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
 
         </div>
--- src/models/Board.ts
+++ src/models/Board.ts
@@ -3,6 +3,9 @@ import { Colors } from "./Colors";
 import { Queen } from "./figures/Queen";
 import { Pawn } from './figures/Pawn';
 import { King } from "./figures/King";
+import { Bishop } from "./figures/Bishop";
+import { Knight } from "./figures/Knight";
+import { Rook } from "./figures/Rook";
 
 export class Board {
     cells: Cell[][] = []
@@ -22,6 +25,23 @@ export class Board {
         }
     }
 
+    public getCopyBoard(): Board {
+        const newBoard = new Board();
+        newBoard.cells = this.cells;
+        return newBoard;
+    }
+
+    public highlightCells(selectedCell: Cell | null) {
+        for(let i = 0; i < this.cells.length; i++) {
+            const row = this.cells[i];
+            for(let j = 0; j < row.length; j++) {
+                const target = row[j];
+                target.available = !!selectedCell?.figure?.canMove(target)
+            }
+        }
+    }
+    
+
     public getCell(x: number, y: number){
         return this.cells[y][x]
     }
@@ -39,19 +59,29 @@ export class Board {
     }
 
     private addQueens() {
-        
+        new Queen(Colors.BLACK, this.getCell(3, 0))
+        new Queen(Colors.WHITE, this.getCell(3, 7))
     }
 
     private addBishops() {
-        
+        new Bishop(Colors.BLACK, this.getCell(2, 0))
+        new Bishop(Colors.BLACK, this.getCell(5, 0))
+        new Bishop(Colors.WHITE, this.getCell(2, 7))
+        new Bishop(Colors.WHITE, this.getCell(5, 7))
     }
 
     private addKnights() {
-        
+        new Knight(Colors.BLACK, this.getCell(1, 0))
+        new Knight(Colors.BLACK, this.getCell(6, 0))
+        new Knight(Colors.WHITE, this.getCell(1, 7))
+        new Knight(Colors.WHITE, this.getCell(6, 7))
     }
 
     private addRook() {
-        
+        new Rook(Colors.BLACK, this.getCell(0, 0))
+        new Rook(Colors.BLACK, this.getCell(7, 0))
+        new Rook(Colors.WHITE, this.getCell(0, 7))
+        new Rook(Colors.WHITE, this.getCell(7, 7))
     }
 
     public addFigures() {
